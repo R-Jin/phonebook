f127404c4cfa3499d885ac211bb3d4d26ff4c402
R-Jin <ryanjin007@gmail.com>
2024-05-25 20:59:01 +0200

    Fix some bugs and also complete 3.11

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 morgan.token('body', (request) => {
     return JSON.stringify(request.body)
 })
@@ -28,6 +29,8 @@ let persons = [
     }
 ]
 
+app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -94,7 +97,7 @@ app.post("/api/persons", (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
