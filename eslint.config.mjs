908016bfba2a784aa26f36ebc8c06ed07563c0af
Ryan Jin <ryanjin007@gmail.com>
2024-05-30 00:15:13 +0200

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
+
